0126713f030e06cec3de6ccd96a785d05b19e146
Kevin Kim <chaeya@gmail.com>
2024-06-18 08:46:10 +0900

    Update inputline pre-edit text for IME

--- plugins/widgetsboxmodel/src/inputline.cpp
+++ plugins/widgetsboxmodel/src/inputline.cpp
@@ -1,20 +1,22 @@
-// Copyright (c) 2022-2024 Manuel Schneider
-
 #include "inputline.h"
 #include <QPaintEvent>
 #include <QPainter>
 #include <QStyleOptionFrame>
+#include <QInputMethodEvent>
+#include <QTextLayout>
 
-InputLine::InputLine(QWidget *parent) : QLineEdit(parent)
+InputLine::InputLine(QWidget *parent) : QLineEdit(parent), trigger_length_(0)
 {
-    connect(this, &QLineEdit::textEdited, this, [this]{
+    connect(this, &QLineEdit::textEdited, this, [this] {
         history_.resetIterator();
         user_text_ = text();
     });
 
     // Clear input hint on text change
-    connect(this, &QLineEdit::textChanged, this,
-            [this](){ input_hint_ = QString(); });
+    connect(this, &QLineEdit::textChanged, this, [this]() { input_hint_ = QString(); });
+
+    // Trigger repaint on cursor movement
+    connect(this, &QLineEdit::cursorPositionChanged, this, [this]() { update(); });
 }
 
 void InputLine::setInputHint(const QString &text)
@@ -54,10 +56,11 @@ void InputLine::paintEvent(QPaintEvent *event)
 
     QRect content_rect = style()->subElementRect(QStyle::SE_LineEditContents, &panel, this);
     content_rect = content_rect.marginsRemoved(textMargins());
-    content_rect.adjust(2,1,-2,-1); // https://codebrowser.dev/qt5/qtbase/src/widgets/widgets/qlineedit_p.cpp.html#QLineEditPrivate::verticalMargin
+    content_rect.adjust(2, 1, -2, -1); // https://codebrowser.dev/qt5/qtbase/src/widgets/widgets/qlineedit_p.cpp.html#QLineEditPrivate::verticalMargin
 
     QPainter p(this);
     p.setPen(panel.palette.placeholderText().color());
+    p.setBackgroundMode(Qt::TransparentMode);
 
     if (!input_hint_.isNull())
     {
@@ -70,31 +73,40 @@ void InputLine::paintEvent(QPaintEvent *event)
         auto fm = fontMetrics();
         auto r = content_rect;
 
-        // Get cursor position in pixels - Change so that hint text does not overlap even in the case of combined letters
-        int cursor_pos = cursorRect().right();
-        
-        // Add offset to avoid overlap
-        int offset = 5;
-        r.setLeft(cursor_pos + offset);
+        // Get the width of the text in pixels, including pre-edit text for IME
+        QString full_text = text() + preedit_text_;
+        int text_width = fm.horizontalAdvance(full_text);
+
+        // Adjust hint starting position to avoid overlap
+        int offset = 20; // Increased offset to avoid overlap during input
+        r.setLeft(r.left() + text_width + offset);
 
         auto t = fm.elidedText(hint, Qt::ElideRight, r.width());
 
         p.drawText(r, Qt::TextSingleLine, t);
     }
-
-    // TODO glitches when line is overflowing, need something better than QLineEdit, maybe QTextEdit
-    // if (trigger_length_)
-    // {
-    //     auto f = p.font();
-    //     f.setUnderline(true);
-    //     p.setFont(f);
-    //     p.drawText(content_rect, Qt::TextSingleLine, text().left(trigger_length_));
-    // }
 }
 
-void InputLine::hideEvent(QHideEvent*)
+void InputLine::hideEvent(QHideEvent *)
 {
     history_.add(text());
     history_.resetIterator();
     user_text_.clear();
+    preedit_text_.clear();
+}
+
+void InputLine::inputMethodEvent(QInputMethodEvent *event)
+{
+    preedit_text_ = event->preeditString();
+    QLineEdit::inputMethodEvent(event);
+    update();
+}
+
+QVariant InputLine::inputMethodQuery(Qt::InputMethodQuery query) const
+{
+    if (query == Qt::ImCursorRectangle)
+        return cursorRect();
+    if (query == Qt::ImSurroundingText)
+        return QVariant(text() + preedit_text_);
+    return QLineEdit::inputMethodQuery(query);
 }
--- plugins/widgetsboxmodel/src/inputline.h
+++ plugins/widgetsboxmodel/src/inputline.h
@@ -3,12 +3,12 @@
 #pragma once
 #include "albert/extension/frontend/inputhistory.h"
 #include <QLineEdit>
+#include <QInputMethodEvent>
 
 // Input method problems: https://bugreports.qt.io/browse/QTBUG-106516
 class InputLine : public QLineEdit
 {
 public:
-
     InputLine(QWidget *parent);
 
     void setInputHint(const QString &text);
@@ -17,14 +17,16 @@ public:
     void next(bool search);
     void previous(bool search);
 
-private:
-
+protected:
     void paintEvent(QPaintEvent *event) override;
     void hideEvent(QHideEvent *event) override;
+    void inputMethodEvent(QInputMethodEvent *event) override;
+    QVariant inputMethodQuery(Qt::InputMethodQuery query) const override;
 
+private:
     QString input_hint_;
     albert::InputHistory history_;
     QString user_text_; // used for history search
     uint trigger_length_;
-
+    QString preedit_text_;
 };
